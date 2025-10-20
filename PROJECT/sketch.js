9cdeadc573de34b5396480eda55c5e21715590d4
ParisGitCode <npofoegbu@outlook.com>
2025-10-20 14:35:37 +0100

    Adjust canvas size and add color toggle on click
    Updated canvas size to fit the window dimensions and modified the draw function to change the ellipse color based on the select variable.

--- PROJECT/sketch.js
+++ PROJECT/sketch.js
@@ -1,7 +1,32 @@
+//function setup() {
+//  createCanvas(400, 400);
+//}
+
+//function draw() {
+//  background(220);
+//}
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  if(select==0){
+    rgb = [255, 0, 0];
+  }else{
+    rgb = [0,0,0];
+  }
+  fill(rgb);
+  ellipse(mouseX, mouseY, 100);
+}
+
+function mouseClicked() {
+  if(select==0){
+    select=1;
+  }else{
+    select=0;
+  }
+  return false;
 }
