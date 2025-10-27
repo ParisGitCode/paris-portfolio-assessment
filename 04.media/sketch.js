5f6fd1d1d1afa762b427c39b2a3de5dff88496cf
ParisGitCode <npofoegbu@outlook.com>
2025-10-27 14:54:25 +0000

    Update sketch.js

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -12,3 +12,26 @@ function draw() {
   background(220);
   image(img, 70, 0);
 }
+
+
+let hum; 
+let img; 
+function preload(){
+  img = loadImage('Antenna.jpg');
+    hum = loadSound("humm.wav");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  capture.hide();
+}
+function draw() {
+  image(img, 0, 0, windowWidth, 500);
+  image(capture, 50, 10, 320, 240);
+  filter(INVERT); 
+}
+function mouseClicked(){
+  hum.loop();
+  hum.amp(0.7); 
+}
