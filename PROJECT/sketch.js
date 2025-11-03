fb838cf5d7218b9eb3d107fefcf113ae5e9d263d
ParisGitCode <npofoegbu@outlook.com>
2025-11-03 09:12:47 -0600

    Enable drawing of random lines and shapes
    Uncommented setup and draw functions to enable drawing random lines and shapes on the canvas.

--- PROJECT/sketch.js
+++ PROJECT/sketch.js
@@ -7,7 +7,7 @@
 //}
 
 
-
+/*
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -29,4 +29,36 @@ function mouseClicked() {
     select=0;
   }
   return false;
+}
+
+*/
+
+// Here I'm creating random lines and shapes
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  frameRate(10);
+}
+
+function draw() {
+  stroke(random(255), random(255), random(255));
+  strokeWeight(random(1, 5));
+
+  let area1 = random(width);
+  let area2 = random(height);
+  let area3 = random(width);
+  let area4 = random(height);
+  line(area1, area2, area3, area4);
+  // I'm using random areas to place the lines and letters
+
+  fill(random(255), random(255), random(255));
+  noStroke();
+
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let char = chars.charAt(floor(random(chars.length)));
+  let tx = random(width);
+  let ty = random(height);
+  text(char, tx, ty);
 }
