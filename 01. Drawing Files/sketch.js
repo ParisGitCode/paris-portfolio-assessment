516be4112d20450e21663d2e71c50782892c6a1e
ParisGitCode <npofoegbu@outlook.com>
2025-11-03 13:11:10 +0000

    Add square drawing to sketch.js
    Fixed syntax errors and added square drawing functionality.

--- 01. Drawing Files/sketch.js
+++ 01. Drawing Files/sketch.js
@@ -6,3 +6,33 @@ function draw() {
   background(220);
   circle(20,50,50)
 }
+
+
+// FOR SQUARES
+
+function setup() {
+  createCanvas(640, 480); 
+  background(230);    
+  // noStroke();              
+}
+
+function draw(){
+    fill(0); 
+    rect(0, 0, 100, 100); // x, y, w, h
+    
+    fill(64);
+		rect(50, 50, 100, 100); 
+
+		fill(128)
+		rect(100, 100, 100, 100);
+		
+		fill(196);
+		rect(150, 150, 200, 200);
+
+		fill(220;
+		rect(300, 300, 400, 400); 
+}
+
+stroke(20, 200, 200);  // r, g, b 
+strokeWeight(10);      // Width of stroke
+fill(200, 20, 200);    // r, g, b
