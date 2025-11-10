c2cb1eb1d79108fc51ee8df486a6fa2f7028c6cb
ParisGitCode <npofoegbu@outlook.com>
2025-11-10 14:56:22 +0000

    Update sketch.js

--- 06 collisions/sketch.js
+++ 06 collisions/sketch.js
@@ -1,14 +1,32 @@
-lt size = 5
+
+let size = 50;
+let x;
+let y; 
+let myFill = 255;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  x=width/2;
+  y=height/2;
 }
 
-function draw() {
-  noFill();
-  ellipse(mouseX, mouseY, size, size)
+function draw(){
+  background(100); 
 
-  size += 5;
-  size = size % 100;
+  fill(myFill);
+  ellipse(x, y, size); 
   
+  fill(150)
+  ellipse(mouseX, mouseY, size); 
+  
+  let distance = dist(x, y, mouseX, mouseY); 
+  if(distance <= size/2){
+    myFill = [255, 0, 0];
+  }else{
+    myFill = 255;
+  }
+
+  fill(0)
+  textSize(15);
+  text(int(distance), x-15, y-10, 30);
 }
